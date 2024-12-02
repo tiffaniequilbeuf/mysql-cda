c76b7d9e286fcdd2e752985a54409346ba1cdb34
Tiffanie Quilbeuf <tiffaniequilbeuf2@gmail.com>
2024-12-02 10:25:25 +0100

    TP01 et TP02

---
+++ tp02-vip-cocktail.sql
@@ -0,0 +1,20 @@
+DROP DATABASE IF EXISTS invitation;
+
+CREATE DATABASE IF NOT EXISTS invitation CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+USE invitation;
+
+CREATE TABLE personne (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    prenom VARCHAR(255) NOT NULL, 
+    nom VARCHAR(255) NOT NULL, 
+    age INT UNSIGNED,
+    date_inscription DATE,
+    etat TINYINT(1) NOT NULL DEFAULT 0,
+    statut ENUM ('membre', 'non-membre') DEFAULT 'non-membre',
+    CV TEXT,
+    salaire_annuel INT UNSIGNED
+)
+ENGINE=InnoDB;
+
+RENAME TABLE personne TO inv_personne;
