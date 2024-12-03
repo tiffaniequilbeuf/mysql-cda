a79898733ba29b028b455b34103b5f81659242e8
Tiffanie Quilbeuf <tiffaniequilbeuf2@gmail.com>
2024-12-03 10:26:15 +0100

    TP07

--- tp07-film-avec-acteurs.sql
+++ tp07-film-avec-acteurs.sql
@@ -22,9 +22,9 @@ ENGINE = InnoDB;
 
 /*D- Création de la table pivot film_has_acteur*/
 CREATE TABLE film_has_acteur (
-    film_id INT,
-    acteur_id INT,
-    PRIMARY KEY (film_id, acteur_id)
+    film_id INT NOT NULL,
+    acteur_id INT NOT NULL,
+    CONSTRAINT pk_film_has_acteur PRIMARY KEY (film_id, acteur_id)
 )
 ENGINE = InnoDB;
 
@@ -36,11 +36,54 @@ ALTER TABLE film_has_acteur ADD CONSTRAINT fk_acteur FOREIGN KEY (acteur_id) REF
     - 1 acteur et film
     - 2 pivot */
 
+INSERT INTO acteur(prenom, nom)
+VALUES 
+    ('Brad', 'PITT'),
+    ('Léonardo', 'DICAPRIO');
+
+INSERT INTO film(nom)
+VALUES 
+    ('Fight Club'),
+    ('One Upon the Time');
+
+INSERT INTO film_has_acteur(film_id, acteur_id)
+VALUES 
+    (1,1),
+    (2,1),
+    (2,2);
+
+
 /*1️⃣ Afficher tous les films de Léonardo DI CAPRIO */
+SELECT 
+film.nom AS film,
+acteur.prenom,
+acteur.nom
+FROM film
+INNER JOIN film_has_acteur  ON film.id = film_has_acteur.film_id
+INNER JOIN acteur ON acteur.id = film_has_acteur.acteur_id
+WHERE acteur.nom = "DICAPRIO";
+
+/*Note : je n'ai pas le même retour */
 
 /*2️⃣ Afficher le nombre de films par acteur */
 
+SELECT COUNT(film.nom) AS nb_film FROM film;    /*J'affiche le nombre de film*/
+SELECT acteur.prenom, acteur.nom FROM acteur;   /*J'affiche la liste des acteurs*/
+
+/*Resultat*/
+SELECT acteur.prenom, acteur.nom, COUNT(film.nom) AS nb_film 
+FROM film
+INNER JOIN film_has_acteur  ON film.id = film_has_acteur.film_id
+INNER JOIN acteur ON acteur.id = film_has_acteur.acteur_id
+GROUP BY acteur.prenom, acteur.nom;
+
 /*3️⃣ Ajouter un film :TITANIC */
+INSERT INTO film(nom)
+VALUES 
+    ('Titanic');
 
 /*4️⃣ Trouver le film qui n'a pas d'acteur */
-
+SELECT film.nom 
+FROM film
+LEFT JOIN film_has_acteur ON film.id = film_has_acteur.film_id
+WHERE film_has_acteur.acteur_id IS NULL;
